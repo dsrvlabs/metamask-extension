7b090355ae2133f7e67754ad52ca113f05377299
Ui Hyeon Lee <ksw09157.5212@gmail.com>
2021-05-12 17:49:37 +0900

    link error (celo explorer) edit

--- ui/lib/account-link.js
+++ ui/lib/account-link.js
@@ -6,23 +6,14 @@ export default function getAccountLink (address, network, rpcPrefs) {
   const net = parseInt(network)
   let link
   switch (net) {
-    case 1: // main net
-      link = `https://etherscan.io/address/${address}`
+    case 42220: // main net
+      link = `https://explorer.celo.org/address/${address}`
       break
-    case 2: // morden test net
-      link = `https://morden.etherscan.io/address/${address}`
+    case 44787: // morden test net
+      link = `https://alfajores-blockscout.celo-testnet.org/address/${address}`
       break
-    case 3: // ropsten test net
-      link = `https://ropsten.etherscan.io/address/${address}`
-      break
-    case 4: // rinkeby test net
-      link = `https://rinkeby.etherscan.io/address/${address}`
-      break
-    case 42: // kovan test net
-      link = `https://kovan.etherscan.io/address/${address}`
-      break
-    case 5: // goerli test net
-      link = `https://goerli.etherscan.io/address/${address}`
+    case 62320: // morden test net
+      link = `https://baklava-blockscout.celo-testnet.org/address/${address}`
       break
     default:
       link = ''
