4be575489b7d91b66868190c0f906809f834af98
Ui Hyeon Lee <ksw09157.5212@gmail.com>
2021-05-20 11:15:48 +0900

    edit token-list disable cGLD

--- ui/app/components/app/token-list/token-list.js
+++ ui/app/components/app/token-list/token-list.js
@@ -6,16 +6,23 @@ import TokenCell from '../token-cell'
 import { useI18nContext } from '../../../hooks/useI18nContext'
 import { useTokenTracker } from '../../../hooks/useTokenTracker'
 import { useSelector } from 'react-redux'
-import { getAssetImages } from '../../../selectors'
+import { getAssetImages, getCurrentNetworkId } from '../../../selectors'
 import { getTokens } from '../../../ducks/metamask/metamask'
 
-export default function TokenList ({ onTokenClick }) {
+const networkAddressChecker = {
+  "42220": "0x471ece3750da237f93b8e339c536989b8978a438",
+  "44787": "0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9",
+  "62320": "0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9"
+}
+
+export default function TokenList({ onTokenClick }) {
   const t = useI18nContext()
   const assetImages = useSelector(getAssetImages)
   // use `isEqual` comparison function because the token array is serialized
   // from the background so it has a new reference with each background update,
   // even if the tokens haven't changed
   const tokens = useSelector(getTokens, isEqual)
+  const networkId = useSelector(getCurrentNetworkId)
   const { loading, error, tokensWithBalances } = useTokenTracker(tokens)
 
   if (loading) {
@@ -36,7 +43,7 @@ export default function TokenList ({ onTokenClick }) {
 
   return (
     <div>
-      {tokensWithBalances.map((tokenData, index) => {
+      {tokensWithBalances.filter(tokenData => networkAddressChecker[networkId].toLowerCase() !== tokenData.address.toLowerCase()).map((tokenData, index) => {
         tokenData.image = assetImages[tokenData.address]
         return (
           <TokenCell
